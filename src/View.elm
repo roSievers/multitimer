3484e33872bee734cf6eeb5fa8c988c09a685ec8
Rolf Sievers <rolf.sievers@posteo.de>
2017-04-04 20:46:22 +0200

    Migrate to Bootstrap and polish the game interface.

--- src/View.elm
+++ src/View.elm
@@ -6,42 +6,72 @@ import Html exposing (Html, div, text, input, button, li, br, ul, span, h1, h2, 
 import Html.Attributes exposing (style, value, disabled, type_, checked)
 import Html.Events exposing (onInput, onClick)
 import Update
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Form as Form
+import Bootstrap.Form.Input as Input
+import Bootstrap.Form.Checkbox as Checkbox
+import Bootstrap.Form.Radio as Radio
+import Bootstrap.Button as Button
+import Bootstrap.Alert as Alert
+import Bootstrap.Card as Card
+import Bootstrap.ListGroup as ListGroup
+import Bootstrap.Badge as Badge
 
 
 page : Model -> Html Msg
 page model =
     case model of
         Ingame gameModel ->
-            game gameModel
+            game gameModel |> wrapBootstrap
 
         Setup setupModel ->
-            setup setupModel
+            setup setupModel |> wrapBootstrap
+
+
+wrapBootstrap : Html Msg -> Html Msg
+wrapBootstrap content =
+    Grid.container []
+        -- Responsive fixed width container
+        [ CDN.stylesheet
+          -- Inlined Bootstrap CSS for use with reactor
+        , content
+        ]
 
 
 setup : SetupModel -> Html Msg
 setup model =
-    div [ style [ ( "margin", "auto" ), ( "max-width", "30em" ) ] ]
-        [ h1 [] [ text "Timer Setup" ]
-        , input [ value model.name_input, onInput NameInput, style [ ( "width", "100%" ) ] ] []
+    Form.form []
+        [ h1 [] [ text "Einstellungen" ]
+        , Input.text
+            [ Input.id "nameInput"
+            , Input.value model.name_input
+            , Input.onInput NameInput
+            , Input.placeholder "Spielernamen mit Komma trennen"
+            ]
+        , br [] []
         , playerList model.name_input
         , br [] []
-        , text "Zeit pro Zug: "
-        , input
-            [ value (toString model.config.buffer_time_initial)
-            , onInput BufferTimeInput
-            , style [ ( "width", "3em" ), ( "margin-right", "1em" ) ]
+        , Form.group []
+            [ Form.label [] [ text "Zeit pro Zug in Sekunden: " ]
+            , Input.number
+                [ Input.value (toString model.config.buffer_time_initial)
+                , Input.onInput BufferTimeInput
+                ]
+            , Checkbox.checkbox
+                [ Checkbox.checked model.config.keep_buffer
+                , Checkbox.onCheck KeepBufferInput
+                , Checkbox.inline
+                ]
+                "Übrige Zeit am Ende vom Zug gutschreiben"
             ]
-            []
-        , checkbox "Übrige Zeit gutschreiben" model.config.keep_buffer KeepBufferInput
-        , br [] []
-        , text "Bedenkzeit: "
-        , input
-            [ value (toString model.time_left)
-            , onInput TimeLeftInput
-            , style [ ( "width", "3em" ), ( "margin-right", "1em" ) ]
+        , Form.group []
+            [ Form.label [] [ text "Bedenkzeit (Insgesammt nach Ablauf der Zugzeit):" ]
+            , Input.number
+                [ Input.value (toString model.time_left)
+                , Input.onInput TimeLeftInput
+                ]
             ]
-            []
-        , checkbox "Passen möglich" model.config.passing_allowed PassInput
         , passingConfig model.config
         , br [] []
         , startGameButton model
@@ -50,20 +80,53 @@ setup model =
 
 passingConfig : Config -> Html Msg
 passingConfig config =
-    div [ style [ displayIf config.passing_allowed, ( "margin-left", "1em" ) ] ]
-        [ checkbox "Nach Passen reagieren" config.passed_playing PassedPlayInput
-        , div [ style [ ( "margin-left", "1em" ), ( "margin-top", "0.5em" ) ] ]
-            [ text "Verminderte Zeit: "
-            , input
-                [ value (toString config.passed_playing_time)
-                , disabled (not (config.passed_playing && config.passing_allowed))
-                , onInput PassedPlayTime
-                , style [ ( "width", "3em" ), ( "margin-right", "1em" ) ]
-                ]
-                []
+    if not config.passing_allowed then
+        enable_passing config
+    else
+        div []
+            [ enable_passing config
+            , passingBody config
             ]
-        , rearrangementChoice config.passing_allowed config.rearrangement
+
+
+passingBody : Config -> Html Msg
+passingBody config =
+    card
+        [ Checkbox.checkbox
+            [ Checkbox.checked config.passed_playing
+            , Checkbox.onCheck PassedPlayInput
+            , Checkbox.inline
+            ]
+            "Nach Passen reagieren"
+        , Form.group []
+            [ Form.label [] [ text "Verminderte Zeit, nach dem Passen:" ]
+            , Input.number
+                [ Input.value (toString config.passed_playing_time)
+                , Input.onInput PassedPlayTime
+                , Input.disabled (not (config.passed_playing && config.passing_allowed))
+                ]
+            ]
+        , rearrangementChoice config.rearrangement
         ]
+
+
+card : List (Html Msg) -> Html Msg
+card msgHtmlList =
+    Card.block []
+        [ Card.custom <| div [] msgHtmlList
+        ]
+        (Card.config [])
+        |> Card.view
+
+
+enable_passing : Config -> Html Msg
+enable_passing config =
+    Checkbox.checkbox
+        [ Checkbox.checked config.passing_allowed
+        , Checkbox.onCheck PassInput
+        , Checkbox.inline
+        ]
+        "Passen möglich"
 
 
 displayIf : Bool -> ( String, String )
@@ -74,14 +137,6 @@ displayIf bool =
       else
         "none"
     )
-
-
-checkbox : String -> Bool -> (Bool -> Msg) -> Html Msg
-checkbox caption current message =
-    label []
-        [ input [ type_ "checkbox", onClick (message (not current)), checked current ] []
-        , text caption
-        ]
 
 
 playerList : String -> Html a
@@ -98,41 +153,53 @@ startGameButton : SetupModel -> Html Msg
 startGameButton setupModel =
     case Update.setupGame setupModel of
         Ok gameModel ->
-            button [ onClick (StartGame gameModel) ] [ text "Timer starten" ]
+            Button.button
+                [ Button.onClick (StartGame gameModel)
+                , Button.primary
+                ]
+                [ text "Timer starten" ]
 
         Err errorText ->
             div []
-                [ button [ disabled True ] [ text "Timer starten" ]
-                , p [ style [ ( "color", "red" ) ] ] [ text errorText ]
+                [ Button.button
+                    [ Button.disabled True
+                    ]
+                    [ text "Timer starten" ]
+                , Alert.danger [ text errorText ]
                 ]
 
 
-rearrangementChoice : Bool -> Rearrangement -> Html Msg
-rearrangementChoice passing_allowed current =
-    div [ style [ ( "margin-top", "0.5em" ) ] ]
-        [ rearrangementButton passing_allowed current Static "Feste Spielreihenfolge"
-        , br [] []
-        , rearrangementButton passing_allowed current StartPlayer "Startspieler nach Passen"
-        , br [] []
-        , rearrangementButton passing_allowed current PassOrder "Komplett nach Passen"
-        ]
-
-
-rearrangementButton : Bool -> Rearrangement -> Rearrangement -> String -> Html Msg
-rearrangementButton passing_allowed current new caption =
+rearrangementChoice : Rearrangement -> Html Msg
+rearrangementChoice current =
     let
-        cssStyles =
-            style
-                [ ( "width", "14em" )
-                , ( "text-align", "left" )
+        choices =
+            [ ( Static, "Feste Spielreihenfolge" )
+            , ( StartPlayer, "Startspieler nach Passen" )
+            , ( PassOrder, "Komplett nach Passen" )
+            ]
+
+        description =
+            case current of
+                Static ->
+                    "Immer der gleiche Spieler darf die neue Runde beginen."
+
+                StartPlayer ->
+                    "Wer als erstes passt darf die neue Runde beginnen. Die Reihenfolge wird beibehalten."
+
+                PassOrder ->
+                    "In der neuen Runde sind die Spieler nach Passreihenfolge sortiert."
+
+        singleChoice ( new, caption ) =
+            Radio.radio
+                [ Radio.onClick (RearrangementInput new)
+                , Radio.checked (current == new)
                 ]
+                caption
     in
-        if current == new then
-            button [ disabled (not passing_allowed), cssStyles ]
-                [ text ("X " ++ caption) ]
-        else
-            button [ onClick (RearrangementInput new), disabled (not passing_allowed), cssStyles ]
-                [ text ("O " ++ caption) ]
+        div []
+            [ Form.group [] (List.map singleChoice choices)
+            , p [] [ text description ]
+            ]
 
 
 
@@ -143,8 +210,7 @@ game : GameModel -> Html Msg
 game model =
     div [ style [ ( "margin", "auto" ), ( "max-width", "30em" ) ] ]
         [ activePlayer model
-        , ul [ style [ ( "clear", "both" ), ( "margin-top", "3em" ) ] ]
-            (List.map waitingPlayer model.players)
+        , ListGroup.ul (List.map (waitingPlayer model.config.rearrangement) model.players)
         , passedPlayers model
         ]
 
@@ -153,20 +219,36 @@ pause : GameModel -> Html Msg
 pause model =
     div [ style [ ( "display", "block" ), ( "float", "right" ) ] ]
         [ if model.paused then
-            button [ onClick (Pause False) ] [ text "Fortfahren" ]
+            Button.button [ Button.onClick (Pause False) ] [ text "Fortfahren" ]
           else
-            button [ onClick (Pause True) ] [ text "Pausieren" ]
+            Button.button [ Button.onClick (Pause True) ] [ text "Pausieren" ]
         ]
 
 
 activePlayer : GameModel -> Html Msg
 activePlayer model =
-    div []
+    card
         [ pause model
-        , h1 [] [ text model.active_player.name ]
+        , playerName model
         , countdown model
         , endTurnButton model
         ]
+
+
+playerName : GameModel -> Html a
+playerName model =
+    case rearrangementBadge model.config.rearrangement model.active_player of
+        Just badge ->
+            h1 []
+                [ text model.active_player.name
+                , text " "
+                , span
+                    [ style [ ( "font-size", "0.5em" ), ( "vertical-align", "middle" ) ] ]
+                    [ badge ]
+                ]
+
+        Nothing ->
+            h1 [] [ text model.active_player.name ]
 
 
 countdown : GameModel -> Html Msg
@@ -220,46 +302,93 @@ endTurnButton : GameModel -> Html Msg
 endTurnButton model =
     let
         passing_disabled =
-            disabled
+            Button.disabled
                 (model.paused
                     || (model.active_player.passed /= Nothing)
                     || not model.config.passing_allowed
                 )
 
+        passing_caption =
+            if (model.active_player.passed /= Nothing) then
+                "Bereits gepasst"
+            else if model.num_passed == List.length model.players + List.length model.disabled_players then
+                "Runde schließen"
+            else
+                "Passen"
+
         end_turn_disabled =
-            disabled model.paused
+            Button.disabled model.paused
     in
         div []
             [ div [ style [ ( "float", "left" ), ( "display", "block" ) ] ]
-                [ button [ passing_disabled, onClick Pass ] [ text "Passen" ] ]
+                [ Button.button [ passing_disabled, Button.onClick Pass ] [ text passing_caption ] ]
             , div [ style [ ( "float", "right" ), ( "display", "block" ) ] ]
-                [ button [ end_turn_disabled, onClick EndTurn ] [ text "Zug beenden" ] ]
+                [ Button.button [ end_turn_disabled, Button.onClick EndTurn, Button.primary ] [ text "Zug beenden" ] ]
             ]
 
 
-waitingPlayer : Player -> Html a
-waitingPlayer player =
-    let
-        color =
-            if player.passed == Nothing then
-                "green"
+waitingPlayer : Rearrangement -> Player -> ListGroup.Item a
+waitingPlayer rearrangement player =
+    ListGroup.li
+        [ playerColor player
+        , ListGroup.attrs [ Html.Attributes.class "justify-content-between" ]
+        ]
+        [ text player.name
+        , playerBadge rearrangement player
+        ]
+
+
+playerColor : Player -> ListGroup.ItemOption a
+playerColor player =
+    case player.passed of
+        Just _ ->
+            ListGroup.danger
+
+        Nothing ->
+            ListGroup.success
+
+
+playerBadge : Rearrangement -> Player -> Html a
+playerBadge rearrangement player =
+    case rearrangementBadge rearrangement player of
+        Just badge ->
+            span []
+                [ badge, text " ", timeBadge player ]
+
+        Nothing ->
+            timeBadge player
+
+
+timeBadge : Player -> Html a
+timeBadge player =
+    Badge.badge []
+        [ text (timeFormat player.time_left) ]
+
+
+rearrangementBadge : Rearrangement -> Player -> Maybe (Html a)
+rearrangementBadge rearrangement player =
+    case rearrangement of
+        Static ->
+            if player.position == 1 then
+                Just <| Badge.pill [] [ text ("start") ]
             else
-                "red"
+                Nothing
 
-        attribute =
-            style [ ( "color", color ) ]
-    in
-        li [ attribute ]
-            [ text player.name
-            , text " -> "
-            , text (timeFormat player.time_left)
-            ]
+        StartPlayer ->
+            if player.passed == Just 1 then
+                Just <| Badge.pill [] [ text ("1.") ]
+            else
+                Nothing
+
+        PassOrder ->
+            player.passed
+                |> Maybe.map (\position -> Badge.pill [] [ text (toString position ++ ".") ])
 
 
 passedPlayers model =
     div [ style [ displayIf (List.length model.disabled_players > 0) ] ]
         [ hr [] []
-        , ul [] (List.map waitingPlayer model.disabled_players)
+        , ListGroup.ul (List.map (waitingPlayer model.config.rearrangement) model.disabled_players)
         ]
 
 
